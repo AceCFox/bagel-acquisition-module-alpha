f167ec019b251502263682ed76ebbde4c264782b
Ace Fox <afox09@gmail.com>
2020-09-23 14:25:46 -0500

    Grocery item input visible

--- Desktop/bagel-acquisition-module-alpha/src/modules/ItemForm.js
+++ Desktop/bagel-acquisition-module-alpha/src/modules/ItemForm.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import {TextField, Button} from '@material-ui/core';
+import React, {useState} from 'react';
+import {TextField, Button, Grid, Paper} from '@material-ui/core';
 
 function ItemForm() {
+    const [input, setInput] = useState({})  
+    
+    const handleInputChange = (e) => setInput({
+        ...input, 
+        [e.currentTarget.name]: e.currentTarget.value
+    })
   return (
-    <div>
-        <TextField required label = "Add Item" defaultValue="Hello World" />
-        <Button>Add</Button>
+    <div style = {{bacgroundColor:'white'}}>
+        <Paper elevation = {3}>
+            <Grid container direction = 'row'>
+                <TextField label = "Grocery Item" variant='filled' name="newItem" onChange={handleInputChange}/>
+                <Button variant = 'contained' color = 'primary'>Add</Button>    
+            </Grid>
+        </Paper>
     </div>
   );
 }
