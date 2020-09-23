c713d7cda1f9e8d814525c93e64dce34549f0e06
Ace Fox <afox09@gmail.com>
2020-09-23 12:32:32 -0500

    successful server spin up

---
+++ Desktop/bagel-acquisition-module-alpha/server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const bodyParser = require('body-parser');;
+
+// Route includes
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* Routes */
+
+// Serve static files
+app.use(express.static('build'));
+
+// App Set //
+const PORT = process.env.PORT || 5000;
+
+/** Listen * */
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
+
+
+module.exports = app;
